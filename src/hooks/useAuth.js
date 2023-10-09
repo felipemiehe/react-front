45e9d5953336c89fcf2eba9df86dc922c7f7e435
Felipe Miehe <felipemiehe555@gmail.com>
2023-10-09 10:54:48 -0300

    Arrumando requisições login

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -4,19 +4,62 @@ import { useLocalStorage } from "./useLocalStorage";
 
 const AuthContext = createContext();
 
+function getCookie(name) {
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.trim().split('=');
+    if (cookieName === name) {
+      return true;
+    }
+  }
+  return null;
+}
+
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useLocalStorage("user", null);
   const navigate = useNavigate();
 
   const login = async (data) => {
-    setUser(data);
-    navigate("/dashboard/profile", { replace: true });
+    const response = await fetch('http://localhost:8000/api/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      credentials: 'include',
+      body: JSON.stringify({
+        email : data.email,
+        password: data.password
+      })
+    });
+
+    if (response.ok) {
+     await checkUser().then(()=>{
+       navigate("/dashboard/profile", { replace: true });
+     })
+    }
   };
 
-  const logout = () => {
+  const checkUser = async () =>{
+    const userRequest = await fetch('http://localhost:8000/api/user', {
+      headers: {'Content-Type': 'application/json'},
+      credentials: 'include',
+    });
+
+    userRequest.ok
+        ? setUser(await userRequest.json())
+        : await logout()
+  }
+
+  const logout = async () => {
+    await fetch('http://localhost:8000/api/logout', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      credentials: 'include',
+    });
+
     setUser(null);
     navigate("/", { replace: true });
-  };
+  }
+
+
 
   const value = useMemo(
     () => ({
